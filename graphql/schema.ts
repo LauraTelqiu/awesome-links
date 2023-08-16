b78e6be5df224330e041be3d704ca9e7dbb69b11
Laura <lauratelqiu@gmail.com>
2023-08-16 10:40:07 -0400

    graphQL API

---
+++ graphql/schema.ts
@@ -0,0 +1,20 @@
+// graphql/schema.ts
+import { builder } from "./builder";
+
+export const schema = builder.toSchema();
+
+// export const typeDefs = `
+//   type Link {
+//     id: ID
+//     title: String
+//     description: String
+//     url: String
+//     category: String
+//     imageUrl: String
+//     users: [String]
+//   }
+
+//   type Query {
+//     links: [Link]!
+//   }
+// `;
